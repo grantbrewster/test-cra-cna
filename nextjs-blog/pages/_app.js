e3f11c5a82e17d0bbb2a585593a44f10ee58e367
Grant Brewster <gbrewster@rippling.com>
2023-03-13 18:13:20 -0400

    adding routes

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,8 @@
+import {useEffect, useState} from 'react';
+
+function App({Component, pageProps}) {
+  const [render, setRender] = useState(false);
+  useEffect(() => setRender(true), []);
+  return render ? <Component {...pageProps} /> : null;
+}
+export default App;
---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import About from '../../src/About';
+import HelloWorld from '../../src/HelloWorld';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/about" Component={About} />
+          <Route path="/" Component={HelloWorld}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
