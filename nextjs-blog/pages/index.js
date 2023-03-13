0079ac9511e69a65ee3114ed2c2b0d76673d7ce8
Grant Brewster <gbrewster@rippling.com>
2023-03-13 18:29:44 -0400

    put it all together now

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-// import About from '../../src/About';
-// import HelloWorld from '../../src/HelloWorld';
+import About from '../../src/About';
+import HelloWorld from '../../src/HelloWorld';
 
 export default function App() {
-  return ( <div>Hello world again</div>
-    // <Router>
-    //   <div>
-    //     <ul>
-    //       <li>
-    //         <Link to="/">Home</Link>
-    //       </li>
-    //       <li>
-    //         <Link to="/about">About</Link>
-    //       </li>
-    //     </ul>
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
 
-    //     {/* <Routes>
-    //       <Route path="/about" Component={About} />
-    //       <Route path="/" Component={HelloWorld}/>
-    //     </Routes> */}
-    //   </div>
-    // </Router>
+        <Routes>
+          <Route path="/about" Component={About} />
+          <Route path="/" Component={HelloWorld}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
